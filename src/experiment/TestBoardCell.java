780bdfdce6a07f4d199aa924cf7da9705d3ff3e6
Mateo Rivera <mateorivera@mines.edu>
2024-10-07 11:49:13 -0600

    Implemented all constructors, setters, and getters for TestBoardCell

--- src/experiment/TestBoardCell.java
+++ src/experiment/TestBoardCell.java
@@ -1,5 +1,47 @@
 package experiment;
 
-public class TestBoardCell {
+import java.util.HashSet;
+import java.util.Set;
 
+public class TestBoardCell {
+	private int row;
+	private int col;
+	private Set<TestBoardCell> adjList;
+	private boolean room;
+	private boolean occupied;
+	
+	
+	public TestBoardCell(int row, int col) {
+		this.row = row;
+		this.col = col;
+		this.adjList = new HashSet<>();
+		this.room = false;
+		this.occupied = false;
+	}
+	
+	public void addAdjacency(TestBoardCell cell) {
+		adjList.add(cell);
+	}
+	
+	public Set<TestBoardCell> getAdjList(){
+		return adjList;
+	}
+	
+	public void setRoom(boolean room) {
+		this.room = room;
+	}
+	
+	public boolean room() {
+		return room;
+	}
+	
+	public void setOccupied(boolean occupied) {
+		this.occupied = occupied;
+	}
+	
+	public boolean getOccupied() {
+		return occupied;
+	}
+	
 }
+
