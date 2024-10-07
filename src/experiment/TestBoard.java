e1a9b99f911dd4d9274eb2a1af8308f2bdf3ceeb
Mateo Rivera <mateorivera@mines.edu>
2024-10-07 13:49:20 -0600

    Created TestBoard class

--- src/experiment/TestBoard.java
+++ src/experiment/TestBoard.java
@@ -1,5 +1,31 @@
 package experiment;
 
-public class TestBoard {
+import java.util.HashSet;
+import java.util.Set;
 
+public class TestBoard {
+	private Set<TestBoardCell> targets;
+	private TestBoardCell[][] grid;
+	
+	public TestBoard() {
+		this.targets = new HashSet<>();
+		grid = new TestBoardCell[4][4];
+		for (int row = 0; row < 4; row++) {
+			for (int col = 0; col < 4; col++) {
+				grid[row][col] = new TestBoardCell(row, col);
+			}
+		}
+	}	
+			
+	public void calcTargets(TestBoardCell startCell, int pathlength) {
+		
+	}
+	
+	public TestBoardCell getCell(int row, int col) {
+		return grid[row][col];
+	}
+	
+	public Set<TestBoardCell> getTargets(){
+		return targets;
+	}
 }
