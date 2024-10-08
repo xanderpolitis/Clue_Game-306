3c676fb0a597f1300f46efab936fd721c97d6a02
mateoriv <mateorivera@mines.edu>
2024-10-07 20:54:22 -0600

    test???

--- src/experiment/TestBoardCell.java
+++ src/experiment/TestBoardCell.java
@@ -9,6 +9,7 @@ public class TestBoardCell {
 	private Set<TestBoardCell> adjList;
 	private boolean room;
 	private boolean occupied;
+	private boolean notOccupied;
 	
 	
 	public TestBoardCell(int row, int col) {
