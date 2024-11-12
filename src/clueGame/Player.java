3ae2a2ebc5c49ca0e5a0bd58856e54bcfb265343
mateoriv <mateorivera@mines.edu>
2024-11-12 09:47:02 -0700

    player 2

--- src/clueGame/Player.java
+++ src/clueGame/Player.java
@@ -3,6 +3,7 @@ package clueGame;
 import java.awt.Color;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Player {
 	private String name;
@@ -16,6 +17,21 @@ public class Player {
 		this.row = row;
 		this.column = column;
 		this.hand = new ArrayList<>();
+	}
+	
+	public Card disproveSuggestion(Solution suggestion) {
+		List<Card> matchingCards = new ArrayList<>();
+		for (Card card : hand) {
+			if (card.getCardName().equals(suggestion.getPerson()) || card.getCardName().equals(suggestion.getWeapon()) || card.getCardName().equals(suggestion.getRoom())) {
+				matchingCards.add(card);
+			}
+		}
+		if (matchingCards.isEmpty()) {
+			return null;
+		} else {
+			Random rand = new Random();
+			return matchingCards.get(rand.nextInt(matchingCards.size()));
+		}
 	}
 	
 	public String getName() {
